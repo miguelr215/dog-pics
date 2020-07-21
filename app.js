43d505a017073a25414c9a49a8e7f9bcba530c87
miguelr215 <66912426+miguelr215@users.noreply.github.com>
2020-07-21 06:28:53 -0700

    updated html, css and js
    images now display to DOM and page and images are responsive

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 // DOG PICS
+let userNumber = 0;
 
+
+// function to display results
+function displayResults(responseJson){
+    console.log(responseJson);
+    // create variable that will hold final image string
+    let finalString = "";
+    // loop through each message and set message to image src
+    for(let i = 0; i < userNumber; i++){
+        finalString = finalString.concat(" ", '<img src="'+ responseJson.message[i] +'" class="dogPic" alt="placeholder"></img>');
+    }
+    // show final string in html
+    $('.picBox').html(finalString);
+};
 
 // function to get dog pic
 function getDogPic(){
-    let userNumber = $('#userNumber').val();
+    userNumber = $('#userNumber').val();
     // set default to 3
     if(userNumber == null || userNumber <= 0 || userNumber === isNaN()){
         userNumber = 3;
     }
     fetch(`https://dog.ceo/api/breeds/image/random/${userNumber}`)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson));
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert('Something went wrong, try again'));
     // clear input
     $('#userNumber').val('');
 };

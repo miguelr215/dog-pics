2aea86da5df98db7dc417832ae89de1f33626b21
miguelr215 <66912426+miguelr215@users.noreply.github.com>
2020-07-22 14:19:53 -0700

    updated with requested revisions

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 // DOG PICS
-let userNumber = 0;
+
 
 
 // function to display results
-function displayResults(responseJson){
+function displayResults(responseJson, numberPics){
+    if(numberPics > responseJson.message.length){
+        numberPics = responseJson.message.length;
+    }
     console.log(responseJson);
     // create variable that will hold final image string
     let finalString = "";
     // loop through each message and set message to image src
-    for(let i = 0; i < userNumber; i++){
+    for(let i = 0; i < numberPics; i++){
         finalString = finalString.concat(" ", '<img src="'+ responseJson.message[i] +'" class="dogPic" alt="placeholder"></img>');
     }
     // show final string in html
+    $('.picBox').removeClass('hidden');
+    $('.picBoxHeader').removeClass('hidden');
     $('.picBox').html(finalString);
 };
 
 // function to get dog pic
 function getDogPic(){
-    userNumber = $('#userNumber').val();
+    let userNumber = $('#userNumber').val();
     // set default to 3
     if(userNumber == null || userNumber <= 0 || userNumber === isNaN()){
         userNumber = 3;
     }
     fetch(`https://dog.ceo/api/breeds/image/random/${userNumber}`)
         .then(response => response.json())
-        .then(responseJson => displayResults(responseJson))
+        .then(responseJson => displayResults(responseJson, userNumber))
         .catch(error => alert('Something went wrong, try again'));
     // clear input
     $('#userNumber').val('');
@@ -32,7 +37,7 @@ function getDogPic(){
 
 // watch form for submit button
 function watchForm(){
-    $('form').on('click', 'button', function(event){
+    $('form').on('click', '#submitBtn', function(event){
         event.preventDefault();
         getDogPic();
     })
